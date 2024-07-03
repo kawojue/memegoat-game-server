ae42221e25695b77f2c896867a7c569a077650cb
Raheem Muyiwa <kawojue08@gmail.com>
2024-07-03 14:24:04 +0100

    Update random.service.ts

--- libs/random.service.ts
+++ libs/random.service.ts
@@ -12,8 +12,28 @@ export class RandomService {
         this.algorithm = algorithm
     }
 
+    uuidv0x() {
+    const value = new Uint8Array(16)
+    crypto.getRandomValues(value)
+
+    const timestamp = BigInt(Date.now())
+    value[0] = Number((timestamp >> 40n) & 0xffn)
+    value[1] = Number((timestamp >> 32n) & 0xffn)
+    value[2] = Number((timestamp >> 24n) & 0xffn)
+    value[3] = Number((timestamp >> 16n) & 0xffn)
+    value[4] = Number((timestamp >> 8n) & 0xffn)
+    value[5] = Number(timestamp & 0xffn)
+
+    value[6] = (value[6] & 0x0f) | 0x70
+    value[8] = (value[8] & 0x3f) | 0x80
+
+    return Array.from(value)
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("")
+}
+
     randomize() {
-        const newSeed = this.hashSeed(`${process.env.GEN_KEY}-${uuidv4()}-${Date.now()}`)
+        const newSeed = this.hashSeed(`${process.env.GEN_KEY}-${this.uuidv0x()}-${Date.now()}`)
         const random = seedrandom(newSeed)()
         return {
             random,
