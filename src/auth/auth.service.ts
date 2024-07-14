2760f292882075ed848ed7d2d33bf37ce576eff6
Raheem Kawojue <kawojue08@gmail.com>
2024-07-14 12:56:12 +0100

    chore: add avatar

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,19 +3,24 @@ import { Response } from 'express'
 import { Injectable } from '@nestjs/common'
 import { MiscService } from 'libs/misc.service'
 import { StatusCodes } from 'enums/StatusCodes'
+import { avatarSeeds } from 'utils/avatar-seeds'
+import { RandomService } from 'libs/random.service'
 import { PrismaService } from 'prisma/prisma.service'
 import { ResponseService } from 'libs/response.service'
 import { ConnectWalletDTO, UsernameDTO } from './dto/auth.dto'
 
 @Injectable()
 export class AuthService {
-    private readonly isProd: boolean
+    private isProd: boolean
+    private randomService: RandomService
+    private readonly avatarBaseUrl = "https://api.dicebear.com/9.x/bottts/svg"
 
     constructor(
         private readonly misc: MiscService,
         private readonly prisma: PrismaService,
         private readonly response: ResponseService,
     ) {
+        this.randomService = new RandomService('md5')
         this.isProd = process.env.NODE_ENV === "production"
     }
 
@@ -28,9 +33,13 @@ export class AuthService {
             if (!user) {
                 const _id = new ObjectId().toString()
 
+                const { random } = this.randomService.randomize()
+                const randomAvatarSeed = avatarSeeds[Math.floor(random * avatarSeeds.length) - 1]
+                const avatarUrl = `${this.avatarBaseUrl}?seed=${randomAvatarSeed}`
+
                 await this.prisma.$transaction([
                     this.prisma.user.create({
-                        data: { id: _id, address }
+                        data: { id: _id, address, avatar: avatarUrl }
                     }),
                     this.prisma.stat.create({
                         data: { user: { connect: { id: _id } } }
@@ -43,20 +52,17 @@ export class AuthService {
                     return this.response.sendError(res, StatusCodes.Forbidden, "Account has been suspended")
                 }
 
-                const payload: JwtPayload = {
+                const access_token = await this.misc.generateAccessToken({
                     sub: user.id,
                     address: user.address
-                }
-
-                const access_token = await this.misc.generateAccessToken(payload)
-
+                })
                 res.cookie('access_token', access_token, {
                     sameSite: this.isProd ? 'none' : 'lax',
                     secure: this.isProd,
-                    maxAge: 10 * 60 * 1000,
+                    maxAge: 120 * 24 * 60 * 60 * 1000,
                 })
 
-                res.redirect('http://localhost:5173/games')
+                res.redirect(process.env.REDIRECT_URL || 'http://localhost:5173/games')
             }
         } catch (err) {
             this.misc.handleServerError(res, err)
---
+++ utils/avatar-seeds.ts
@@ -0,0 +1,5 @@
+export const avatarSeeds = [
+    "Missy", "Maggie", "Princess", "Peanut", "Willow", "Cookie",
+    "Snuggles", "Oreo", "Milo", "Tiger", "Oscar", "Sadie", "Mia",
+    "Sheba", "Bear", "Sophie", "Oliver", "Chloe", "Midnight", "Kiki",
+]
