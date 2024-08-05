d1a889c6ee58639d963288471b2faf52870428a5
JoE11-y <Josephedoh77@gmail.com>
2024-08-05 18:58:14 +0100

    add extra fields to transactions

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -5,37 +5,38 @@ import {
   BadRequestException,
   UnauthorizedException,
   InternalServerErrorException,
-} from '@nestjs/common'
-import * as crypto from 'crypto'
-import { Request } from 'express'
-import { ApiTags } from '@nestjs/swagger'
-import { WebhookService } from './webhook.service'
+} from '@nestjs/common';
+import * as crypto from 'crypto';
+import { Request } from 'express';
+import { ApiTags } from '@nestjs/swagger';
+import { WebhookService } from './webhook.service';
 
-@ApiTags("Webhook")
+@ApiTags('Webhook')
 @Controller('webhook')
 export class WebhookController {
-  constructor(private readonly webhookService: WebhookService) { }
+  constructor(private readonly webhookService: WebhookService) {}
 
   @Post()
   async receiveWebhook(@Req() req: Request) {
     if (!req.body || !req.body?.event || !req.body?.data) {
-      throw new BadRequestException('Invalid request body received')
+      throw new BadRequestException('Invalid request body received');
     }
 
-    const signature = req.headers['x-webhook-signature']
-    const hashedSignature = crypto.createHmac('sha512', process.env.WEBHOOK_SECRET)
+    const signature = req.headers['x-webhook-signature'];
+    const hashedSignature = crypto
+      .createHmac('sha512', process.env.WEBHOOK_SECRET)
       .update(JSON.stringify(req.body))
-      .digest('hex')
+      .digest('hex');
 
     if (signature !== hashedSignature) {
-      throw new UnauthorizedException("Invalid signature received")
+      throw new UnauthorizedException('Invalid signature received');
     }
 
     try {
-      await this.webhookService.enqueueRequest(req)
+      await this.webhookService.enqueueRequest(req);
     } catch (err) {
-      console.error(err)
-      throw new InternalServerErrorException()
+      console.error(err);
+      throw new InternalServerErrorException();
     }
   }
 }
