06136f938246b8c2b6ae573e55db8df3db6ffb4e
Raheem Muyiwa <kawojue08@gmail.com>
2024-08-25 15:22:27 +0100

    Update custom-auth.guard.middleware.ts

--- src/middlewares/custom-auth.guard.middleware.ts
+++ src/middlewares/custom-auth.guard.middleware.ts
@@ -1,16 +1,17 @@
 import { JwtService } from '@nestjs/jwt'
 import { env } from 'configs/env.config'
-import { NestMiddleware } from '@nestjs/common'
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
 
-export class CustomAuthMiddlware implements NestMiddleware {
-    constructor(private readonly jwtService: JwtService) { }
+@Injectable()
+export class CustomAuthMiddleware implements NestMiddleware {
+    constructor(private readonly jwtService: JwtService) {}
 
     private async validateAndDecodeToken(token: string) {
         try {
             return await this.jwtService.verifyAsync(token, {
                 ignoreExpiration: false,
-                secret: env.jwt.secret
+                secret: env.jwt.secret,
             })
         } catch {
             return null
@@ -18,18 +19,23 @@ export class CustomAuthMiddlware implements NestMiddleware {
     }
 
     async use(req: Request, res: Response, next: NextFunction) {
-        let bearerToken: string
+        let token: string | undefined
+
         const authHeader = req.headers.authorization
         const cookieToken = req.cookies?.access_token
-        const token = cookieToken || authHeader
 
-        if (authHeader) {
-            bearerToken = authHeader.split(' ')[1]
+        if (cookieToken) {
+            token = cookieToken
+        } else if (authHeader && authHeader.startsWith('Bearer ')) {
+            token = authHeader.split(' ')[1]
         }
+
         if (token) {
             const decodedToken = await this.validateAndDecodeToken(token)
             if (decodedToken) {
                 req.user = decodedToken
+            } else {
+                throw new UnauthorizedException('Invalid or expired token')
             }
         }
 
