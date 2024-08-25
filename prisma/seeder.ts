6f13c238cc6c639495f988f983c991b6c9470d9a
Raheem Kawojue <kawojue08@gmail.com>
2024-08-26 00:50:11 +0100

    delete all tournament

---
+++ prisma/seeder.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const seedTournament = async () => {
+    await prisma.tournament.deleteMany()
+
+
+    await prisma.$disconnect()
+}
+
+seedTournament()
