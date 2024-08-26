f1a5fe5b811e30fc4b4f61c9b6ed9df5afd9c0d0
Raheem Kawojue <kawojue08@gmail.com>
2024-08-26 15:35:17 +0100

    feat: signature verification

--- prisma/seeder.ts
+++ prisma/seeder.ts
@@ -1,9 +1,11 @@
-// const fs = require('fs')
-// import { PrismaClient } from '@prisma/client'
+const fs = require('fs')
+import { v4 as uuidv4 } from 'uuid'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
 const seeder = async () => {
+
     console.log("Yay!")
 }
 
