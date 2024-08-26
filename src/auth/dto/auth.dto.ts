ce377ea528618719bd77e6c84ff95bb6770d337d
JoE11-y <Josephedoh77@gmail.com>
2024-08-26 13:34:00 +0100

    update sign in

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,25 +1,47 @@
-import { USER_REGEX } from 'utils/regExp'
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator'
+import { USER_REGEX } from 'utils/regExp';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 
 export class ConnectWalletDTO {
-    @ApiProperty({
-        example: 'x0cd..'
-    })
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(255)
-    address: string
+  @ApiProperty({
+    example: 'x0cd..',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  address: string;
+
+  @ApiProperty({
+    example: 'b3x0cd..',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  publicKey: string;
+
+  @ApiProperty({
+    example: 'b3x0cd..',
+  })
+  @IsString()
+  @IsNotEmpty()
+  signature: string;
+
+  @ApiProperty({
+    example: 'signature req..',
+  })
+  @IsString()
+  @IsNotEmpty()
+  message: string;
 }
 
 export class UsernameDTO {
-    @ApiProperty({
-        example: "kawojue_"
-    })
-    @Matches(USER_REGEX, {
-        message: "Invalid username"
-    })
-    @IsString()
-    @IsNotEmpty()
-    username: string
+  @ApiProperty({
+    example: 'kawojue_',
+  })
+  @Matches(USER_REGEX, {
+    message: 'Invalid username',
+  })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
 }
