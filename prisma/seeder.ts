1d6917df16dca2ac73942469539597f41035fe2c
Raheem Kawojue <kawojue08@gmail.com>
2024-08-26 01:38:47 +0100

    push seeder

--- prisma/seeder.ts
+++ prisma/seeder.ts
@@ -7,6 +7,13 @@ const seedTournament = async () => {
 
     await prisma.round.deleteMany()
 
+    await prisma.stat.updateMany({
+        data: {
+            total_wins: 0,
+            total_points: 0,
+            total_losses: 0,
+        }
+    })
 
     await prisma.$disconnect()
 }
