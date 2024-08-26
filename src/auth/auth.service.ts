9f1f6e545804a8187a8677074551852dd997b718
JoE11-y <Josephedoh77@gmail.com>
2024-08-26 16:05:22 +0100

    add logs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { ObjectId } from 'mongodb';
 import { Response } from 'express';
 import { env } from 'configs/env.config';
-import { enc, HmacSHA256 } from 'crypto-js'
+import { enc, HmacSHA256 } from 'crypto-js';
 import { MiscService } from 'libs/misc.service';
 import { StatusCodes } from 'enums/StatusCodes';
 import { avatarSeeds } from 'utils/avatar-seeds';
@@ -10,165 +10,172 @@ import { PrismaService } from 'prisma/prisma.service';
 import { ResponseService } from 'libs/response.service';
 import { ConnectWalletDTO, UsernameDTO } from './dto/auth.dto';
 import { verifyMessageSignatureRsv } from '@stacks/encryption';
-import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-    private randomService: RandomService;
-    private readonly avatarBaseUrl = 'https://api.dicebear.com/9.x/bottts/svg';
+  private randomService: RandomService;
+  private readonly avatarBaseUrl = 'https://api.dicebear.com/9.x/bottts/svg';
 
-    constructor(
-        private readonly misc: MiscService,
-        private readonly prisma: PrismaService,
-        private readonly response: ResponseService,
-    ) {
-        this.randomService = new RandomService('md5');
+  constructor(
+    private readonly misc: MiscService,
+    private readonly prisma: PrismaService,
+    private readonly response: ResponseService,
+  ) {
+    this.randomService = new RandomService('md5');
+  }
+
+  async verifySignature(receivedSignature: string, receivedTimestamp: string) {
+    const clientSecret = env.auth.key;
+
+    const expectedSignature = HmacSHA256(receivedTimestamp, clientSecret);
+    console.log(expectedSignature);
+
+    const encodedExpectedSignature = enc.Base64.stringify(expectedSignature);
+    console.log(encodedExpectedSignature);
+
+    if (encodedExpectedSignature !== receivedSignature) {
+      throw new Error('Invalid signature');
     }
 
-    async verifySignature(receivedSignature: string, receivedTimestamp: string) {
-        const clientSecret = env.auth.key
+    const currentTime = Date.now();
+    const receivedTime = parseInt(receivedTimestamp, 10);
+    const timeDifference = currentTime - receivedTime;
 
-        const expectedSignature = HmacSHA256(receivedTimestamp, clientSecret)
-        const encodedExpectedSignature = enc.Base64.stringify(expectedSignature)
-
-        if (encodedExpectedSignature !== receivedSignature) {
-            throw new Error("Invalid signature")
-        }
-
-        const currentTime = Date.now()
-        const receivedTime = parseInt(receivedTimestamp, 10)
-        const timeDifference = currentTime - receivedTime
-
-        if (timeDifference > 300000) {
-            throw new ForbiddenException("Timestamp is too old")
-        }
-
-        return true
+    if (timeDifference > 300000) {
+      throw new ForbiddenException('Timestamp is too old');
     }
 
-    async connectWallet({
-        address,
-        signature,
+    return true;
+  }
+
+  async connectWallet({
+    address,
+    signature,
+    message,
+    publicKey,
+  }: ConnectWalletDTO) {
+    try {
+      const isVerified = verifyMessageSignatureRsv({
         message,
         publicKey,
-    }: ConnectWalletDTO) {
-        try {
-            const isVerified = verifyMessageSignatureRsv({
-                message,
-                publicKey,
-                signature,
-            });
+        signature,
+      });
 
-            if (!isVerified) {
-                throw new ForbiddenException('Signature is invalid');
-            }
+      if (!isVerified) {
+        throw new ForbiddenException('Signature is invalid');
+      }
 
-            const user = await this.prisma.user.findUnique({
-                where: { address },
-            });
+      const user = await this.prisma.user.findUnique({
+        where: { address },
+      });
 
-            if (!user) {
-                const _id = new ObjectId().toString();
+      if (!user) {
+        const _id = new ObjectId().toString();
 
-                const { random } = this.randomService.randomize();
-                const randomAvatarSeed =
-                    avatarSeeds[Math.floor(random * avatarSeeds.length) - 1];
-                const avatarUrl = `${this.avatarBaseUrl}?seed=${randomAvatarSeed}`;
+        const { random } = this.randomService.randomize();
+        const randomAvatarSeed =
+          avatarSeeds[Math.floor(random * avatarSeeds.length) - 1];
+        const avatarUrl = `${this.avatarBaseUrl}?seed=${randomAvatarSeed}`;
 
-                await this.prisma.$transaction([
-                    this.prisma.user.create({
-                        data: { id: _id, address, avatar: avatarUrl },
-                    }),
-                    this.prisma.stat.create({
-                        data: {
-                            tickets: 100_000,
-                            user: { connect: { id: _id } },
-                        },
-                    }),
-                ]);
-            }
-
-            if (user) {
-                if (!user.active) {
-                    throw new ForbiddenException('Account has been suspended');
-                }
-            }
-
-            const parsedMessage = JSON.parse(message)
-
-            const requestId = parsedMessage?.requestId
-            const issuedAt = parsedMessage?.issuedAt
-
-            const signatureVerified = await this.verifySignature(requestId, issuedAt)
-
-            if (signatureVerified) {
-                throw new UnauthorizedException("Invalid Signature")
-            }
-
-            const access_token = await this.misc.generateAccessToken({
-                sub: user.id,
-                address: user.address,
-            });
-
-            return { access_token, user };
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    async editUsername(
-        res: Response,
-        { sub }: ExpressUser,
-        { username }: UsernameDTO,
-    ) {
-        try {
-            const user = await this.prisma.user.findFirst({
-                where: {
-                    username: { equals: username, mode: 'insensitive' }
-                },
-            });
-
-            if (user) {
-                return this.response.sendError(
-                    res,
-                    StatusCodes.Conflict,
-                    'Username has been taken',
-                );
-            }
-
-            await this.prisma.user.update({
-                where: { id: sub },
-                data: { username },
-            });
-
-            this.response.sendSuccess(res, StatusCodes.OK, {
-                data: { username },
-                message: 'Username has been updated successfully',
-            });
-        } catch (err) {
-            this.misc.handleServerError(res, err);
-        }
-    }
-
-    async profile(res: Response, { sub }: ExpressUser) {
-        const user = await this.prisma.user.findUnique({
-            where: { id: sub },
-            include: {
-                stat: true,
-                _count: {
-                    select: {
-                        rounds: true,
-                    },
-                },
+        await this.prisma.$transaction([
+          this.prisma.user.create({
+            data: { id: _id, address, avatar: avatarUrl },
+          }),
+          this.prisma.stat.create({
+            data: {
+              tickets: 100_000,
+              user: { connect: { id: _id } },
             },
-        });
+          }),
+        ]);
+      }
 
-        const newUser = {
-            ...user,
-            times_played: user._count.rounds,
-            _count: undefined,
-        };
+      if (user) {
+        if (!user.active) {
+          throw new ForbiddenException('Account has been suspended');
+        }
+      }
 
-        this.response.sendSuccess(res, StatusCodes.OK, { data: newUser });
+      const parsedMessage = JSON.parse(message);
+
+      const requestId = parsedMessage?.requestId;
+      const issuedAt = parsedMessage?.issuedAt;
+
+      const signatureVerified = await this.verifySignature(requestId, issuedAt);
+
+      if (signatureVerified) {
+        throw new UnauthorizedException('Invalid Signature');
+      }
+
+      const access_token = await this.misc.generateAccessToken({
+        sub: user.id,
+        address: user.address,
+      });
+
+      return { access_token, user };
+    } catch (err) {
+      throw err;
     }
+  }
+
+  async editUsername(
+    res: Response,
+    { sub }: ExpressUser,
+    { username }: UsernameDTO,
+  ) {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: {
+          username: { equals: username, mode: 'insensitive' },
+        },
+      });
+
+      if (user) {
+        return this.response.sendError(
+          res,
+          StatusCodes.Conflict,
+          'Username has been taken',
+        );
+      }
+
+      await this.prisma.user.update({
+        where: { id: sub },
+        data: { username },
+      });
+
+      this.response.sendSuccess(res, StatusCodes.OK, {
+        data: { username },
+        message: 'Username has been updated successfully',
+      });
+    } catch (err) {
+      this.misc.handleServerError(res, err);
+    }
+  }
+
+  async profile(res: Response, { sub }: ExpressUser) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: sub },
+      include: {
+        stat: true,
+        _count: {
+          select: {
+            rounds: true,
+          },
+        },
+      },
+    });
+
+    const newUser = {
+      ...user,
+      times_played: user._count.rounds,
+      _count: undefined,
+    };
+
+    this.response.sendSuccess(res, StatusCodes.OK, { data: newUser });
+  }
 }
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,258 +1,264 @@
-import { Response } from 'express'
-import { Injectable } from '@nestjs/common'
-import { StatusCodes } from 'enums/StatusCodes'
-import { PaginationDTO } from './dto/pagination'
-import { PrismaService } from 'prisma/prisma.service'
-import { ResponseService } from 'libs/response.service'
+import { Response } from 'express';
+import { Injectable } from '@nestjs/common';
+import { StatusCodes } from 'enums/StatusCodes';
+import { PaginationDTO } from './dto/pagination';
+import { PrismaService } from 'prisma/prisma.service';
+import { ResponseService } from 'libs/response.service';
 
 @Injectable()
 export class GamesService {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly response: ResponseService,
-    ) { }
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly response: ResponseService,
+  ) {}
 
-    async buyTicket(res: Response, { sub }: ExpressUser) {
-        /* Some Web3 CONTRACT TRANSACTIONS STUFF HERE */
+  async buyTicket(res: Response, { sub }: ExpressUser) {
+    /* Some Web3 CONTRACT TRANSACTIONS STUFF HERE */
 
-        /* Assuming 100 tickets equal $0.05 */
+    /* Assuming 100 tickets equal $0.05 */
 
-        const qty = 100
+    const qty = 100;
 
-        await this.prisma.stat.update({
-            where: { userId: sub },
-            data: { tickets: { increment: qty } }
-        })
+    await this.prisma.stat.update({
+      where: { userId: sub },
+      data: { tickets: { increment: qty } },
+    });
 
-        this.response.sendSuccess(res, StatusCodes.OK, {})
+    this.response.sendSuccess(res, StatusCodes.OK, {});
+  }
+
+  async overallLeaderboard({ limit = 50, page = 1 }: PaginationDTO) {
+    page = Number(page);
+    limit = Number(limit);
+
+    const offset = (page - 1) * limit;
+
+    const totalUsers = await this.prisma.user.count({
+      where: { active: true },
+    });
+
+    const { _sum } = await this.prisma.stat.aggregate({
+      where: { user: { active: true } },
+      _sum: { total_points: true },
+    });
+
+    const leaderboard = await this.prisma.user.findMany({
+      where: { active: true },
+      select: {
+        id: true,
+        stat: {
+          select: {
+            total_points: true,
+          },
+        },
+        avatar: true,
+        address: true,
+        username: true,
+      },
+      orderBy: {
+        stat: {
+          total_points: 'desc',
+        },
+      },
+      take: limit,
+      skip: offset,
+    });
+
+    const totalPages = Math.ceil(totalUsers / limit);
+    const hasNext = page < totalPages;
+    const hasPrev = page > 1;
+
+    return {
+      totalPoints: _sum,
+      leaderboard,
+      totalPages,
+      hasNext,
+      hasPrev,
+    };
+  }
+
+  async getCurrentTournamentLeaderboard({
+    limit = 50,
+    page = 1,
+  }: PaginationDTO) {
+    page = Number(page);
+    limit = Number(limit);
+
+    const offset = (page - 1) * limit;
+
+    const currentTournament = await this.prisma.tournament.findFirst({
+      where: {
+        start: { lte: new Date() },
+        end: { gte: new Date() },
+      },
+    });
+
+    if (!currentTournament) {
+      return { leaderboard: [], totalPages: 0, hasNext: false, hasPrev: false };
     }
 
-    async overallLeaderboard({ limit = 50, page = 1 }: PaginationDTO) {
-        page = Number(page)
-        limit = Number(limit)
-
-        const offset = (page - 1) * limit
-
-        const totalUsers = await this.prisma.user.count({
-            where: { active: true },
-        })
-
-        const { _sum } = await this.prisma.stat.aggregate({
-            where: { user: { active: true } },
-            _sum: { total_points: true }
-        })
-
-        const leaderboard = await this.prisma.user.findMany({
-            where: { active: true },
-            select: {
-                id: true,
-                stat: {
-                    select: {
-                        total_points: true,
-                    },
-                },
-                avatar: true,
-                address: true,
-                username: true,
+    const totalUsers = await this.prisma.user.count({
+      where: {
+        active: true,
+        rounds: {
+          some: {
+            createdAt: {
+              gte: currentTournament.start,
+              lte: currentTournament.end,
             },
-            orderBy: {
-                stat: {
-                    total_points: 'desc',
-                },
+          },
+        },
+      },
+    });
+
+    const leaderboard = await this.prisma.user.findMany({
+      where: {
+        active: true,
+        rounds: {
+          some: {
+            createdAt: {
+              gte: currentTournament.start,
+              lte: currentTournament.end,
             },
-            take: limit,
-            skip: offset,
-        })
+          },
+        },
+      },
+      select: {
+        id: true,
+        avatar: true,
+        address: true,
+        username: true,
+        rounds: {
+          where: {
+            createdAt: {
+              gte: currentTournament.start,
+              lte: currentTournament.end,
+            },
+          },
+          select: {
+            point: true,
+          },
+        },
+      },
+    });
 
-        const totalPages = Math.ceil(totalUsers / limit)
-        const hasNext = page < totalPages
-        const hasPrev = page > 1
-
+    let totalTournamentPoints = 0;
+    const sortedLeaderboard = leaderboard
+      .map((user) => {
+        const totalPoints = user.rounds.reduce(
+          (acc, round) => acc + round.point,
+          0,
+        );
+        totalTournamentPoints += totalPoints;
         return {
-            totalPoints: _sum,
-            leaderboard,
-            totalPages,
-            hasNext,
-            hasPrev,
-        }
-    }
+          ...user,
+          totalPoints,
+          totalRounds: user.rounds.length,
+          rounds: undefined,
+        };
+      })
+      .sort((a, b) => b.totalPoints - a.totalPoints)
+      .slice(offset, offset + limit);
 
-    async getCurrentTournamentLeaderboard({ limit = 50, page = 1 }: PaginationDTO) {
-        page = Number(page)
-        limit = Number(limit)
+    const totalPages = Math.ceil(totalUsers / limit);
+    const hasNext = page < totalPages;
+    const hasPrev = page > 1;
 
-        const offset = (page - 1) * limit
+    return {
+      hasNext,
+      hasPrev,
+      totalUsers,
+      totalPages,
+      currentTournament,
+      totalTournamentPoints,
+      leaderboard: sortedLeaderboard,
+    };
+  }
 
-        const currentTournament = await this.prisma.tournament.findFirst({
-            where: {
-                start: { lte: new Date() },
-                end: { gte: new Date() },
-            },
-        })
+  async overallPosition(userId?: string) {
+    let userTotalPoints = 0;
+    let userPosition: null | number = null;
 
-        if (!currentTournament) {
-            return { leaderboard: [], totalPages: 0, hasNext: false, hasPrev: false }
-        }
-
-        const totalUsers = await this.prisma.user.count({
-            where: {
-                active: true,
-                rounds: {
-                    some: {
-                        createdAt: {
-                            gte: currentTournament.start,
-                            lte: currentTournament.end,
-                        },
-                    },
-                },
-            },
-        })
-
-        const leaderboard = await this.prisma.user.findMany({
-            where: {
-                active: true,
-                rounds: {
-                    some: {
-                        createdAt: {
-                            gte: currentTournament.start,
-                            lte: currentTournament.end,
-                        },
-                    },
-                },
-            },
+    if (userId) {
+      const userStat = await this.prisma.user.findUnique({
+        where: { id: userId },
+        select: {
+          stat: {
             select: {
-                id: true,
-                avatar: true,
-                address: true,
-                username: true,
-                rounds: {
-                    where: {
-                        createdAt: {
-                            gte: currentTournament.start,
-                            lte: currentTournament.end,
-                        },
-                    },
-                    select: {
-                        point: true,
-                    },
-                },
+              total_points: true,
             },
-        })
+          },
+        },
+      });
 
-        let totalTournamentPoints = 0
-        const sortedLeaderboard = leaderboard
-            .map((user) => {
-                const totalPoints = user.rounds.reduce((acc, round) => acc + round.point, 0)
-                totalTournamentPoints += totalPoints
-                return {
-                    ...user,
-                    totalPoints,
-                    totalRounds: user.rounds.length,
-                    rounds: undefined,
-                }
-            })
-            .sort((a, b) => b.totalPoints - a.totalPoints)
-            .slice(offset, offset + limit)
+      userTotalPoints = userStat.stat.total_points;
 
-        const totalPages = Math.ceil(totalUsers / limit)
-        const hasNext = page < totalPages
-        const hasPrev = page > 1
-
-        return {
-            hasNext,
-            hasPrev,
-            totalUsers,
-            totalPages,
-            currentTournament,
-            totalTournamentPoints,
-            leaderboard: sortedLeaderboard,
-        }
+      userPosition = await this.prisma.user.count({
+        where: {
+          active: true,
+          stat: {
+            total_points: {
+              gte: userTotalPoints,
+            },
+          },
+        },
+      });
     }
 
-    async overallPosition(userId?: string) {
-        let userTotalPoints = 0
-        let userPosition: null | number = null
+    return { userPosition, userTotalPoints };
+  }
 
-        if (userId) {
-            const userStat = await this.prisma.user.findUnique({
-                where: { id: userId },
-                select: {
-                    stat: {
-                        select: {
-                            total_points: true,
-                        },
-                    },
+  async tournamentPosition(userId?: string) {
+    let position = 0;
+    let userPoints = 0;
+
+    if (userId) {
+      const currentTournament = await this.prisma.tournament.findFirst({
+        where: {
+          paused: false,
+          start: { lte: new Date() },
+          end: { gte: new Date() },
+        },
+      });
+
+      if (!currentTournament) {
+        return null;
+      }
+
+      const userScores = await this.prisma.round.groupBy({
+        by: ['userId'],
+        _sum: {
+          point: true,
+        },
+        where: {
+          user: {
+            rounds: {
+              some: {
+                createdAt: {
+                  gte: currentTournament.start,
+                  lte: currentTournament.end,
                 },
-            })
+              },
+            },
+          },
+        },
+        orderBy: {
+          _sum: {
+            point: 'desc',
+          },
+        },
+      });
 
-            userTotalPoints = userStat.stat.total_points
+      const userEntry = userScores.find((user) => user.userId === userId);
 
-            userPosition = await this.prisma.user.count({
-                where: {
-                    active: true,
-                    stat: {
-                        total_points: {
-                            gte: userTotalPoints,
-                        },
-                    },
-                },
-            })
-        }
+      if (userEntry) {
+        userPoints = userEntry._sum.point;
+        position = userScores.findIndex((user) => user.userId === userId) + 1;
+      }
 
-        return { userPosition, userTotalPoints }
+      position = position > 0 ? position : null;
     }
 
-    async tournamentPosition(userId?: string) {
-        let position = 0
-        let userPoints = 0
-
-        if (userId) {
-            const currentTournament = await this.prisma.tournament.findFirst({
-                where: {
-                    paused: false,
-                    start: { lte: new Date() },
-                    end: { gte: new Date() },
-                },
-            })
-
-            if (!currentTournament) {
-                return null
-            }
-
-            const userScores = await this.prisma.round.groupBy({
-                by: ['userId'],
-                _sum: {
-                    point: true,
-                },
-                where: {
-                    user: {
-                        rounds: {
-                            some: {
-                                createdAt: {
-                                    gte: currentTournament.start,
-                                    lte: currentTournament.end,
-                                },
-                            },
-                        },
-                    },
-                },
-                orderBy: {
-                    _sum: {
-                        point: 'desc',
-                    },
-                },
-            })
-
-            const userEntry = userScores.find((user) => user.userId === userId)
-
-            if (userEntry) {
-                userPoints = userEntry._sum.point
-                position = userScores.findIndex((user) => user.userId === userId) + 1
-            }
-
-            position = position > 0 ? position : null
-        }
-
-        return { position, userPoints }
-    }
+    return { position, userPoints };
+  }
 }
