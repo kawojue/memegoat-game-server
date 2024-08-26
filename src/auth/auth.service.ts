25c9b3dc4e6f4fecd076fbeb46fe3224bbffd5b7
JoE11-y <Josephedoh77@gmail.com>
2024-08-26 13:55:36 +0100

    update request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,130 +13,127 @@ import { verifyMessageSignatureRsv, hashMessage } from '@stacks/encryption';
 
 @Injectable()
 export class AuthService {
-    private randomService: RandomService;
-    private readonly avatarBaseUrl = 'https://api.dicebear.com/9.x/bottts/svg';
+  private randomService: RandomService;
+  private readonly avatarBaseUrl = 'https://api.dicebear.com/9.x/bottts/svg';
 
-    constructor(
-        private readonly misc: MiscService,
-        private readonly prisma: PrismaService,
-        private readonly response: ResponseService,
-    ) {
-        this.randomService = new RandomService('md5');
-    }
+  constructor(
+    private readonly misc: MiscService,
+    private readonly prisma: PrismaService,
+    private readonly response: ResponseService,
+  ) {
+    this.randomService = new RandomService('md5');
+  }
 
-    async connectWallet({
-        address,
-        signature,
+  async connectWallet({
+    address,
+    signature,
+    message,
+    publicKey,
+  }: ConnectWalletDTO) {
+    try {
+      const isVerified = verifyMessageSignatureRsv({
         message,
         publicKey,
-    }: ConnectWalletDTO) {
-        try {
-            const hashedMessage = bytesToHex(hashMessage(message));
-            const isVerified = verifyMessageSignatureRsv({
-                message: hashedMessage,
-                publicKey,
-                signature,
-            });
+        signature,
+      });
 
-            if (!isVerified) {
-                throw new ForbiddenException('Signature is invalid');
-            }
+      if (!isVerified) {
+        throw new ForbiddenException('Signature is invalid');
+      }
 
-            const user = await this.prisma.user.findUnique({
-                where: { address },
-            });
+      const user = await this.prisma.user.findUnique({
+        where: { address },
+      });
 
-            if (!user) {
-                const _id = new ObjectId().toString();
+      if (!user) {
+        const _id = new ObjectId().toString();
 
-                const { random } = this.randomService.randomize();
-                const randomAvatarSeed =
-                    avatarSeeds[Math.floor(random * avatarSeeds.length) - 1];
-                const avatarUrl = `${this.avatarBaseUrl}?seed=${randomAvatarSeed}`;
+        const { random } = this.randomService.randomize();
+        const randomAvatarSeed =
+          avatarSeeds[Math.floor(random * avatarSeeds.length) - 1];
+        const avatarUrl = `${this.avatarBaseUrl}?seed=${randomAvatarSeed}`;
 
-                await this.prisma.$transaction([
-                    this.prisma.user.create({
-                        data: { id: _id, address, avatar: avatarUrl },
-                    }),
-                    this.prisma.stat.create({
-                        data: {
-                            tickets: 1_000,
-                            user: { connect: { id: _id } },
-                        },
-                    }),
-                ]);
-            }
-
-            if (user) {
-                if (!user.active) {
-                    throw new ForbiddenException('Account has been suspended');
-                }
-
-
-            }
-
-            const access_token = await this.misc.generateAccessToken({
-                sub: user.id,
-                address: user.address,
-            });
-
-            return { access_token, user };
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    async editUsername(
-        res: Response,
-        { sub }: ExpressUser,
-        { username }: UsernameDTO,
-    ) {
-        try {
-            const user = await this.prisma.user.findUnique({
-                where: { username },
-            });
-
-            if (user) {
-                return this.response.sendError(
-                    res,
-                    StatusCodes.Conflict,
-                    'Username has been taken',
-                );
-            }
-
-            await this.prisma.user.update({
-                where: { id: sub },
-                data: { username },
-            });
-
-            this.response.sendSuccess(res, StatusCodes.OK, {
-                data: { username },
-                message: 'Username has been updated successfully',
-            });
-        } catch (err) {
-            this.misc.handleServerError(res, err);
-        }
-    }
-
-    async profile(res: Response, { sub }: ExpressUser) {
-        const user = await this.prisma.user.findUnique({
-            where: { id: sub },
-            include: {
-                stat: true,
-                _count: {
-                    select: {
-                        rounds: true,
-                    },
-                },
+        await this.prisma.$transaction([
+          this.prisma.user.create({
+            data: { id: _id, address, avatar: avatarUrl },
+          }),
+          this.prisma.stat.create({
+            data: {
+              tickets: 1_000,
+              user: { connect: { id: _id } },
             },
-        });
+          }),
+        ]);
+      }
 
-        const newUser = {
-            ...user,
-            times_played: user._count.rounds,
-            _count: undefined,
-        };
+      if (user) {
+        if (!user.active) {
+          throw new ForbiddenException('Account has been suspended');
+        }
+      }
 
-        this.response.sendSuccess(res, StatusCodes.OK, { data: newUser });
+      const access_token = await this.misc.generateAccessToken({
+        sub: user.id,
+        address: user.address,
+      });
+
+      return { access_token, user };
+    } catch (err) {
+      throw err;
     }
+  }
+
+  async editUsername(
+    res: Response,
+    { sub }: ExpressUser,
+    { username }: UsernameDTO,
+  ) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { username },
+      });
+
+      if (user) {
+        return this.response.sendError(
+          res,
+          StatusCodes.Conflict,
+          'Username has been taken',
+        );
+      }
+
+      await this.prisma.user.update({
+        where: { id: sub },
+        data: { username },
+      });
+
+      this.response.sendSuccess(res, StatusCodes.OK, {
+        data: { username },
+        message: 'Username has been updated successfully',
+      });
+    } catch (err) {
+      this.misc.handleServerError(res, err);
+    }
+  }
+
+  async profile(res: Response, { sub }: ExpressUser) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: sub },
+      include: {
+        stat: true,
+        _count: {
+          select: {
+            rounds: true,
+          },
+        },
+      },
+    });
+
+    const newUser = {
+      ...user,
+      times_played: user._count.rounds,
+      _count: undefined,
+    };
+
+    this.response.sendSuccess(res, StatusCodes.OK, { data: newUser });
+  }
 }
