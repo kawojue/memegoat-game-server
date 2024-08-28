892637a12c7ccba33929cc9af81d51815928a998
Raheem Kawojue <kawojue08@gmail.com>
2024-08-28 21:15:34 +0100

    fix: rng

--- rng.ts
+++ rng.ts
@@ -1,4 +1,4 @@
-import { axios } from "axios";
+import axios from "axios";
 import { cvToValue, ClarityValue } from "@stacks/transactions";
 
 type apiResponse = {
@@ -15,19 +15,19 @@ let config = {
   method: 'post',
   maxBodyLength: Infinity,
   url: 'https://api.hiro.so/v2/contracts/call-read/SP2F4QC563WN0A0949WPH5W1YXVC4M1R46QKE0G14/memegoat-lottery-rng/get-final-number',
-  headers: { 
-    'Accept-Encoding': 'gzip, deflate, br, zstd', 
+  headers: {
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Content-Type': 'application/json'
   },
-  data : data
+  data: data
 };
 
 axios.request(config)
-.then((response) => {
-  const result = (response.data as apiResponse).result;
-  const number = cvToValue(result as ClarityValue).value;
-  console.log(number);
-})
-.catch((error) => {
-  console.log(error);
-});
+  .then((response) => {
+    const result = (response.data as apiResponse).result;
+    const number = cvToValue(result as unknown as ClarityValue).value;
+    console.log(number);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
