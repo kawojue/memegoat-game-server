e982733d04aa4886d0c68779e835ea155f349906
JoE11-y <Josephedoh77@gmail.com>
2024-09-28 15:51:05 +0100

    finalize log

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -22,9 +22,8 @@ export class AppController {
   @Get('/test')
   async getBlockHeight(@Res() res: Response) {
     const data = await this.apiService.getCurrentBlock<any>(env.hiro.channel);
-    const result = data.results;
-    console.log(result);
-    console.log(data.results['0'], 'sdjfk');
-    return this.response.sendSuccess(res, StatusCodes.OK, result);
+    const result = data.results['0'].burn_block_height;
+    console.log(result, 'sdjfk');
+    return this.response.sendSuccess(res, StatusCodes.OK, { res: result });
   }
 }
