e693f7f7b146350cd1309174533b3782e5804bf3
JoE11-y <Josephedoh77@gmail.com>
2024-10-01 09:25:49 +0100

    show logs

--- src/queue/transactions.processor.ts
+++ src/queue/transactions.processor.ts
@@ -73,6 +73,7 @@ export class TransactionsQueueProcessor extends WorkerHost {
               txnInfo.contract_call.contract_id === env.hiro.contractId ||
               txnInfo.contract_call.contract_id === env.hiro.goatTokenId
             ) {
+              console.log(txnInfo);
               await this.prisma.$transaction(async (prisma) => {
                 let amount = 0;
                 let txMeta: any;
